c0227f9cb7346f9093c52a6b0a83096e857985c1
Tran Van Duong <boop.devops@gmail.com>
2025-10-22 00:30:13 +0700

    feat: build docker compose for the development environment

---
+++ frontend-user/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: [process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS || '']
+};
+
+export default nextConfig;
